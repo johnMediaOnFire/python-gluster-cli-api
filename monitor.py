9ed90a2b13e41d1fb5d9c0c2918bf25ff79816ce
Phil Kauffman <kauffman@cs.uchicago.edu>
2015-05-07 15:38:32 -0500

    give same features as old monitoring script

--- monitor.py
+++ monitor.py
@@ -4,9 +4,22 @@ import gluster
 if __name__ == '__main__':
 
   ret = gluster.peer.status()
-
   non_connected_peers = []
 
   for peer in ret['cliOutput']['peerStatus']['peer']:
-    if not peer['connected']:
+    if int(peer['connected']) != 1:
        non_connected_peers.append(peer['hostname'])
+
+  ret = gluster.volume.info()
+  non_running_volumes = []
+
+  for volume in ret['cliOutput']['volInfo']['volumes']['volume']:
+    if int(volume['status']) != 1:
+      non_running_volumes.append(volume['name'])
+
+# Output
+  for peer in non_connected_peers:
+    print 'Peer "' + peer + '" is not connected to the pool.'
+
+  for volume in non_running_volumes:
+    print 'Volume "' + volume + '" is not running.'
